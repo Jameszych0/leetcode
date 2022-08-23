682dc998cb84f3d3f377f334cb26c6e19aef99f4
James <1771232307@qq.com>
2022-08-23 23:16:22 +0800

    两两交换链表中的节点

--- src/medium/linklist/SwapNodesinPairs24.java
+++ src/medium/linklist/SwapNodesinPairs24.java
@@ -2,7 +2,19 @@ package medium.linklist;
 
 public class SwapNodesinPairs24 {
     public ListNode swapPairs(ListNode head) {
-
-        return head;
+        // 创建虚拟头节点
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode cur = dummyHead;
+        // 限制条件，遍历到最后，偶数第二个条件，奇数第一个条件
+        while (cur.next != null && cur.next.next != null) {
+            ListNode temp1 = cur.next; // 防止要交换的第一个节点丢失
+            ListNode temp2 = cur.next.next.next; // 防止第二个节点后一个节点消失
+            cur.next = cur.next.next; // 把要交换的前一个节点指向第二个节点
+            cur.next.next = temp1; // 把第二个节点指向第一个节点
+            cur.next.next.next = temp2; // 把第一个节点指向第二个节点的后一个节点
+            cur = cur.next.next; // cur向后移两位
+        }
+        return dummyHead.next;
     }
 }
