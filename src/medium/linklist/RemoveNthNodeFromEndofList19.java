e14c79b4fae1459c5c30800c0755ae105d1e8ec5
James <1771232307@qq.com>
2022-08-24 09:58:21 +0800

    删除链表的倒数第N个节点

---
+++ src/medium/linklist/RemoveNthNodeFromEndofList19.java
@@ -0,0 +1,26 @@
+package medium.linklist;
+
+public class RemoveNthNodeFromEndofList19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        //fast ---_--
+        //slow --_---
+        //相当于slow走了 链表总数size - n
+        ListNode fast = dummyHead;
+        ListNode slow = dummyHead;
+        //fast ---_--
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+        //slow --_---
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        //删除节点
+        slow.next = slow.next.next;
+
+        return dummyHead.next;
+    }
+}
