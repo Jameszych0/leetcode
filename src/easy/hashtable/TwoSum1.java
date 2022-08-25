af4173b46858601d9f4c41531f645c5d2cb396d1
James <1771232307@qq.com>
2022-08-25 20:32:28 +0800

    两数之和

--- src/easy/hashtable/TwoSum1.java
+++ src/easy/hashtable/TwoSum1.java
@@ -1,5 +1,18 @@
 package easy.hashtable;
 
+import java.util.HashMap;
+
 public class TwoSum1 {
-    
+    public int[] twoSum(int[] nums, int target) {
+        // Map存放值和下标
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            // 查询i前有没有值匹配，如有直接返回两个值，没有的话，就把值存进map
+            if (hashMap.containsKey(target - nums[i])) {
+                return new int[] {hashMap.get(target - nums[i]), i};
+            }
+            hashMap.put(nums[i], i);
+        }
+        return new int[0];
+    }
 }
