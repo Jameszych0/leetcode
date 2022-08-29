0f62cbe7b950da236d5e89c4f0ceacf6738281d0
James <1771232307@qq.com>
2022-08-29 13:31:26 +0800

    重复的子字符串

--- src/easy/string/KMPRepeatedSubstringPattern459.java
+++ src/easy/string/KMPRepeatedSubstringPattern459.java
@@ -1,6 +1,6 @@
 package easy.string;
 
-public class RepeatedSubstringPattern459 {
+public class KMPRepeatedSubstringPattern459 {
     /*
      * 移动匹配
      * 如果s由重复的字符串构成，那两个s掐头去尾，中间一定有一个s
@@ -12,7 +12,9 @@ public class RepeatedSubstringPattern459 {
 
     /*
      * kmp算法
-     * 前缀和里最大的最长前后缀
+     * 前缀和里最大的最长前后缀不包含的就是最小重复子串
+     * 根据KMP算法求得最小重复子串的长度除以字符串的长度，如果等于0返回true
+     * 注意最长前后缀等于0的情况
      */
     public boolean repeatedSubstringPattern_way2(String s) {
         int[] next = new int[s.length()];
