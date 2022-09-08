a22328add022ec5d87c69858f8b94cc43f4493ba
James <1771232307@qq.com>
2022-09-08 15:46:04 +0800

    二叉树的前序遍历

--- src/easy/binarytree/BinaryTreePreorderTraversal144.java
+++ src/easy/binarytree/BinaryTreePreorderTraversal144.java
@@ -1,8 +1,21 @@
 package easy.binarytree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BinaryTreePreorderTraversal144 {
-       public List<Integer> preorderTraversal(TreeNode root) {
-        
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        preorder(root, res);
+        return res;
     }
- 
+
+    public void preorder(TreeNode root, List<Integer>res){
+        if(root == null)
+            return ;
+        res.add(root.val);
+        preorder(root.left, res);
+        preorder(root.right, res);
+    }
+
 }
