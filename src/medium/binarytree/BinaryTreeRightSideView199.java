106e2b3d52e0ebc4f7b0c7af998e72932a32150f
James <1771232307@qq.com>
2022-09-13 20:16:29 +0800

    二叉树的右视图

--- src/medium/binarytree/BinaryTreeRightSideView199.java
+++ src/medium/binarytree/BinaryTreeRightSideView199.java
@@ -1,5 +1,32 @@
 package medium.binarytree;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
 public class BinaryTreeRightSideView199 {
-    
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.offer(root);
+// 注意只有两个节点的情况
+        while (!deque.isEmpty()) {
+            int len = deque.size();
+            while (len > 0) {
+                TreeNode node = deque.poll();
+                if (node.left != null)
+                    deque.offer(node.left);
+                if (node.right != null)
+                    deque.offer(node.right);
+                // 当len为一时，就是树的最右边        
+                if (len == 1)
+                    res.add(node.val);
+                len--;
+            }
+        }
+        return res;
+    }
 }
