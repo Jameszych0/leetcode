dcd9a4577c43b553cc15bb190be275191a7f6f44
James <1771232307@qq.com>
2022-11-14 08:32:39 +0800

    全排列

---
+++ src/medium/backtracking/Permutations46.java
@@ -0,0 +1,35 @@
+package medium.backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Permutations46 {
+    List<List<Integer>> res = new ArrayList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+    boolean[] used;
+    public List<List<Integer>> permute(int[] nums) {
+        used = new boolean[nums.length];
+        Arrays.fill(used, false);
+        backtracking(nums, used);
+        return res;
+    }
+
+    void backtracking(int[] nums, boolean[] used) {
+        if (path.size() == nums.length) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i] == true)
+                continue;
+            used[i] = true;
+            path.add(nums[i]);
+            backtracking(nums, used);
+            path.removeLast();
+            used[i] = false;
+        }
+    }
+}
