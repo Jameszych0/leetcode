1ea47442304596b4043be8180c3fec28a3ab2e38
James <1771232307@qq.com>
2022-12-01 11:41:14 +0800

    零钱兑换II

--- src/medium/dp/CoinChangeII518.java
+++ src/medium/dp/CoinChangeII518.java
@@ -5,6 +5,7 @@ public class CoinChangeII518 {
         int[] dp = new int[amount + 1];
         // dp数组初始化
         dp[0] = 1;
+        // 遍历顺序先物品后背包就是组合问题，物品只会经过一次
         for (int i = 0; i < coins.length; i++)
             for (int j = coins[i]; j <= amount; j++)
                 // 算方法的时候就直接相加
