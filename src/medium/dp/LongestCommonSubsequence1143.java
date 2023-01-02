5dfd7fc108d34d8e40cde1f26eb03110be485692
James <1771232307@qq.com>
2023-01-03 07:56:53 +0800

    最长公共子序列

--- src/medium/dp/LongestCommonSubsequence1143.java
+++ src/medium/dp/LongestCommonSubsequence1143.java
@@ -3,6 +3,7 @@ package medium.dp;
 public class LongestCommonSubsequence1143 {
     public int longestCommonSubsequence(String text1, String text2) {
         int len1 = text1.length(), len2 = text2.length();
+        // 长度为[0, i - 1]的字符串text1与长度为[0, j - 1]的字符串text2的最长公共子序列为dp[i][j]
         int[][] dp = new int[len1 + 1][len2 + 1];
         for (int i = 1; i <= len1; i++) {
             char char1 = text1.charAt(i - 1);
