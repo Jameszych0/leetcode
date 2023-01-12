38c09937a878ff4445dfc05fe07e33d50b187130
James <1771232307@qq.com>
2023-01-12 09:15:59 +0800

    摆动序列

--- src/medium/greedy/WiggleSubsequence376.java
+++ src/medium/greedy/WiggleSubsequence376.java
@@ -8,8 +8,16 @@ public class WiggleSubsequence376 {
      */
     // greedy
     public int wiggleMaxLength01(int[] nums) {
-
-        return 0;
+        int preDiff = 0, curDiff = 0;
+        int res = 1;
+        for (int i = 1; i < nums.length; i++) {
+            curDiff = nums[i] - nums[i - 1];
+            if ((curDiff > 0 && preDiff <= 0) || (curDiff < 0 && preDiff >= 0)) {
+                preDiff = curDiff;
+                res++;
+            }
+        }
+        return res;
     }
 
     // dp
