6560b516156043a53274b9d6ca78078bd2646016
James <1771232307@qq.com>
2023-01-19 16:54:12 +0800

    柠檬水找零

--- src/easy/greedy/LemonadeChange860.java
+++ src/easy/greedy/LemonadeChange860.java
@@ -2,8 +2,24 @@ package easy.greedy;
 
 public class LemonadeChange860 {
     public boolean lemonadeChange(int[] bills) {
-
+        int five = 0, ten = 0;
+        for (int i = 0; i < bills.length; i++) {
+            if (bills[i] == 5)
+                five++;
+            if (bills[i] == 10) {
+                    ten++;
+                    five--;
+            }
+            if (bills[i] == 20) {
+                if (ten > 0) {
+                    ten--;
+                    five--;
+                } else
+                    five -= 3;
+            }
+            if (five < 0 || ten < 0)
+                return false;
+        }
         return true;
     }
-
 }
