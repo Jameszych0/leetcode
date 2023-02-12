df4bd109376be2d095f992de0c1043edf72ebe8c
James <1771232307@qq.com>
2023-02-12 09:28:48 +0800

    比较含退格的字符串

---
+++ src/easy/string/BackspaceStringCompare844.java
@@ -0,0 +1,23 @@
+package easy.string;
+
+public class BackspaceStringCompare844 {
+    // 栈
+    public boolean backspaceCompare(String s, String t) {
+        StringBuilder ssb = new StringBuilder();
+        StringBuilder tsb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c != '#')
+                ssb.append(c);
+            else if (ssb.length() > 0)
+                ssb.deleteCharAt(ssb.length() - 1);
+        }
+        for (char c : t.toCharArray()) {
+            if (c != '#')
+                tsb.append(c);
+            else if (tsb.length() > 0)
+                tsb.deleteCharAt(tsb.length() - 1);
+        }
+        return ssb.toString().equals(tsb.toString());
+    }
+
+}
