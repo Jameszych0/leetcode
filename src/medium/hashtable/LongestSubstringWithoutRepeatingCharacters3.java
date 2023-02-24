ab5976aa4c041395e9aa8d81c54744cc0847ce87
James <1771232307@qq.com>
2023-02-24 08:50:06 +0800

    无重复字符的最长子串

---
+++ src/medium/hashtable/LongestSubstringWithoutRepeatingCharacters3.java
@@ -0,0 +1,21 @@
+package medium.hashtable;
+
+import java.util.HashMap;
+
+public class LongestSubstringWithoutRepeatingCharacters3 {
+    // 滑动窗口
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0;
+        int maxLen = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i)))
+                // 如果left大就表示出现的字符不在滑动窗口中
+                // 反之，我们就让left从map中出现的字符的后一位开始
+                left = Math.max(left, map.get(s.charAt(i)) + 1);
+            map.put(s.charAt(i), i);
+            maxLen = Math.max(maxLen, i - left + 1);
+        }
+        return maxLen;
+    }
+}
